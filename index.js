193966e5c86696fbc7804cfe4c9c2cc4350f79f5
Tim Shipman <bricklayertim@yahoo.com>
2017-11-14 17:11:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solutions in this file
-const workers = ['Lisa', 'Kaitlin', 'Jan']
+let workers = ['Lisa', 'Kaitlin', 'Jan']
 
 function printBadges(){
   for (let index = 0; i < workers.length; index++) {
