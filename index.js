e794446b1cec6ca816066686299707ad12c25582
Tim Shipman <bricklayertim@yahoo.com>
2017-11-14 17:13:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Code your solutions in this file
-var workers = ['Lisa', 'Kaitlin', 'Jan']
+const workers = ['Lisa', 'Kaitlin', 'Jan']
 
 function printBadges(){
-  for (let index = 0; i < workers.length; index++) {
+  for (let i = 0; i < workers.length; i++) {
     console.log(`Welcome ${}! You are employee #1.`)
   }
-  return index
+  return 
 }
